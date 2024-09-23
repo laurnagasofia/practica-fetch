69425151691a9311b17d1b220942bdcb2ac0e1b9
laurnagaSofia <laurnagasofia@gmail.com>
2024-09-23 11:00:25 -0300

    Update script.js
    Modifico el código ya existente y agrego fetch()

--- js/script.js
+++ js/script.js
@@ -10,12 +10,20 @@ const container = document.getElementById("container"); // "Traemos" utilizando 
  * Los datos se mostrarán dentro del div de id "container" y por cada ítem se está creando un nuevo párrafo donde se
  * imprime el campo "name" y el campo "lastname" separados por un espacio
  */
-function showData(dataArray) {
+function showData(DATA_URL) {
   // El for itera sobre los elementos del array
-  for (const item of dataArray) {
+  for (const student of DATA_URL) {
     // En la siguiente línea se utilizan "backticks" para armar el String. Más info => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Template_literals
-    container.innerHTML += `<p> ${item.name} ${item.lastname} </p>`; // Se concatena cada párrafo de la manera que queremos mostrarlo al innerHTML del contenedor
+    container.innerHTML += `<p> ${student.name} ${student.lastname} </p>`; // Se concatena cada párrafo de la manera que queremos mostrarlo al innerHTML del contenedor
   }
 }
 
+fetch(DATA_URL)
+.then (response => response.json())
+.then(data => {
+  showData(data.students); // Llama a showData con la lista de estudiantes
+  })
+  .catch(error => {
+    console.error('Hubo un problema al hacer fetch', error);
+  });
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
